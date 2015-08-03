aae27ffc78edc77b07188be3e320fab1e1ad8d05
craigmelbourne <craig.melbourne@gmail.com>
2015-08-03 16:53:15 +0100

    hades

--- hcom/apollo/hcom-services.js
+++ hcom/apollo/hcom-services.js
@@ -53,11 +53,23 @@ var monthNames = [ "Jan", "Feb", "Mar", "Apr", "May", "June",
     var coMonth = co.getMonth() + 2;
     var coYear = co.getFullYear();
 
-// set currency 
+// set search parameters 
 var currency = "GBP";
 var pos = "uk";
 
-function buildSearchUrl(destination) {
+console.log(ciMonth + ", " + ciDay )
+
+ciMonth = "9";
+ciDay = "4";
+
+coMonth = "9";
+coDay = "8";
+
+var destination = (!getParameterByName("destination")) ? 'London' : getParameterByName("destination");
+var numAdults = (!getParameterByName("numAdults")) ? '2' : getParameterByName("numAdults");
+var numChildren = (!getParameterByName("numChildren")) ? '0' : getParameterByName("numChildren");
+
+/*function buildSearchUrl(destination) {
 
 
     // build query string
@@ -67,15 +79,49 @@ function buildSearchUrl(destination) {
     var url = '/api/' + pos + '/search.html?'+qStr;
 
     return url;
-}
+}*/
+
+var buildSearchUrl = function(destination) {
+
+      var qStr = ""
+    // build query string
+      qStr += 'destination='+destination
+      qStr += '&cur='+ currency 
+      qStr += '&monthCheckIn='+ciMonth+'&dayInMonthCheckIn='+ciDay+'&monthCheckOut='+coMonth+'&dayInMonthCheckOut='+coDay;
+      qStr +='&r=1' 
+      qStr += '&roomInfoList[0].numberOfAdults=' + numAdults;
+      qStr += '&roomInfoList[0].numberOfChildren=' + numChildren;
+      //qStr += '&roomInfoList[0].childrenAges[0]=9';
+      qStr += '&na=os%3DFacebook%7Ccv%3D1171%7Csv%3D2%7Cid%3DCFUUID9BC82AC4A123485586F5F6AFEF2084E0'
+
+      // create API URL
+      var url = '/api/' + pos + '/search.html?'+qStr;
+
+      return url;
+    }
 
 function buildHotelDetailsUrl(id) {
 
   // build query string
   var qStr = 'hotelId='+ id + '&cur='+ currency + '&monthCheckIn=' + ciMonth + '&dayInMonthCheckIn=' + ciDay + '&monthCheckOut=' + coMonth + '&dayInMonthCheckOut=' + coDay + '&rooms[0].numberOfAdults=2&roomno=1&reviewOrder=date_newest_first&na=os%3DFacebook%7Ccv%3D1171%7Csv%3D2%7Cid%3DCFUUID9BC82AC4A123485586F5F6AFEF2084E0'
   
+  var qStr1 = ""
+
+  qStr1 += 'hotelId='+ id;
+  qStr1 += '&cur='+ currency;
+  qStr1 += '&monthCheckIn=' + ciMonth;
+  qStr1 += '&dayInMonthCheckIn=' + ciDay;
+  qStr1 += '&monthCheckOut=' + coMonth;
+  qStr1 += '&dayInMonthCheckOut=' + coDay;
+  qStr1 += '&roomno=1';
+  qStr1 += '&rooms[0].numberOfAdults=' + numAdults;
+  qStr1 += '&children[0]=' + numChildren;
+  qStr1 += '&reviewOrder=date_newest_first&na=os%3DFacebook%7Ccv%3D1171%7Csv%3D2%7Cid%3DCFUUID9BC82AC4A123485586F5F6AFEF2084E0'
+
+
   // create API URL  
-  var url = '/api/'+ pos +'/hotelDetails.html?' + qStr;
+  var url = '/api/'+ pos +'/hotelDetails.html?' + qStr1;
+  console.log(url);
 
   return url;
 }
